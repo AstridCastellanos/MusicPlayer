d1f24eafc92376af02b5dea5267abd8b482918dc
AstridCastellanos <astridcorado88@gmail.com>
2023-02-02 15:17:13 -0600

    Cambiar canción con scroll

--- songs.js
+++ songs.js
@@ -1,5 +1,6 @@
 let i=0, playState=false, repeatState=false, farwardState=false, loopState=false, pista, minutes = 0, secunds = 0, minutesdos = 0, secundsdos = 0, duracion, duraciondos, songImg, audio;
 const maximo =  300;
+let previous, estado = false, stop = false, prevStatus = false, nextStatus = false, firstTime = false;
 
 const wrapper = document.querySelector('.wrapper');
 const playButton = document.querySelector('.play-button');
@@ -16,6 +17,7 @@ const containerAudio = document.querySelector('.container-audio')
 const farwardButton = document.querySelector('.farward-button');
 const backwardButton = document.querySelector('.backward-button');
 
+const tittleP = document.querySelector('.tittle');
 const container = document.querySelector('.slider-container');
 const containerCards = document.querySelector('.cards');
 const songTitle = document.querySelector('.song');
@@ -76,7 +78,7 @@ songsList.push({
     filemp3: './music/ADAN JFW  CORRIDO DE YERI MUA AUDIO OFICIAL.mp3',
     fileogv: './music//ADAN JFW CORRIDO DE YERI MUA AUDIO OFICIAL.ogv',
     songDuration: '2:57',
-    songCode:'006'
+    songCode:'004'
 });
 
 songsList.push({
@@ -86,7 +88,7 @@ songsList.push({
     filemp3: './music/CHUNG HA  Snapping MV.mp3',
     fileogv: './music/CHUNG HA Snapping MV.ogv',
     songDuration: '3:34',
-    songCode:'004'
+    songCode:'005'
 });
 
 songsList.push({
@@ -96,27 +98,78 @@ songsList.push({
     filemp3: './music/Ariana Grande  break up with your girlfriend im bored Official Video.mp3',
     fileogv: './music/Ariana Grande break up with your girlfriend im bored Official Video.ogv',
     songDuration: '3:24',
-    songCode:'005'
+    songCode:'006'
 });
 
 //Método para renderizar la imagen de las canciones
 function renderImg(array){
-    let u = 0;
+    let u = 1;
     
     for (song of array){
         
         const songCard = document.createElement('div');
         songCard.classList.add('item');
-
+        songCard.setAttribute('position', u);
+        u++;
         songImg = document.createElement('img'); 
-        songImg.setAttribute('code', song.songCode);
         songImg.setAttribute('src', song.image); 
-        songImg.addEventListener('click', playSong);
+        //songImg.addEventListener('click', playSong);
         
         songCard.appendChild(songImg);
         container.appendChild(songCard);
     }
+
+    const centerImage = (entradas, observador) => {
+        entradas.forEach((entrada) => {
+            if(entrada.isIntersecting){
+                if(estado){
+                    removeId();
+                    const actual = containerCards.scrollLeft;
+                    
+                    if(actual>=previous){
+                        //Hacia el final
+                        console.log('Hacia el final');
+                        const newWidth = (wrapper.clientWidth-250)/2;
+                        containerCards.scrollLeft += newWidth+25;
+                    }else {
+                        //Hacia el inicio
+                        console.log('Hacia el inicio');
+                        const newWidth = (wrapper.clientWidth-250)/2;
+                        containerCards.scrollLeft -= newWidth;
+                    }
+                    const position = (entrada.target.getAttribute('position'))-1;
+                    render(songsList[position], position);
+                    stop = true;
+                }
+            }else {
+                entrada.target.removeAttribute('id');
+                estado = true;
+            }
+            previous = containerCards.scrollLeft;
+        });
+    }
+
+
+    const observador = new IntersectionObserver(centerImage, {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: 1.0
+    });
+    
+    for(i = 0; i < container.children.length; i++) {
+        observador.observe(container.children[i]);
+    }
 }
+
+if(stop){
+    setInterval(stopScroll, 2000);
+}
+
+function stopScroll(){
+    const positioX = containerCards.scrollLeft;
+    containerCards.onscroll = function () { containerCards.scrollTo(positioX, 0); };
+}
+
 
 //Método para remover el id que tiene estilos para que se haga más grande la imagen de la canción que se está reproduciendo
 function removeId() {
@@ -128,10 +181,13 @@ function removeId() {
 //Método para renderizar con la información de la canción seleccionada al presionar en alguna imagen
 function playSong(event){
 
+    console.log(containerCards.scrollLeft);
     const result = songsList.find(cancion => cancion.songCode === event.target.getAttribute('code'));
     const indexSong = songsList.findIndex(cancion => cancion.songCode === event.target.getAttribute('code'));
 
     render(result, indexSong);
+    
+    console.log(containerCards.scrollLeft);
 }
 
 //Método para que se reproduzca el audio
@@ -235,6 +291,7 @@ function tim(position) {
         endD.innerHTML = minutes.toString() + ":" + secunds.toString(); 
     }
 }*/
+
 //Método que se mantiene actualizando los números del tiempo de reproducción de la canción, además repite la canción o toda la lista dependiendo de los botones presionados.
 function time() {
     seg = audio.currentTime;
@@ -280,20 +337,17 @@ function time() {
 
 //Método que reproduce la canción siguiente
 function nextSong(){
-
     const indexSong = songsList.findIndex(cancion => cancion.songCode === audio.getAttribute('code'));
     
     if(indexSong+1 < songsList.length){
         const result = songsList[indexSong+1];
-        render(result, indexSong+1);
-        let rect = container.getBoundingClientRect();
-
-        containerCards.scrollLeft += 250;//(width:210) + (margin-left: 15) + (margin-righ:15) + (margin-left: 15 (next card))
+        observerFunction(indexSong, 'next');
+        //(width:210) + (margin-left: 15) + (margin-righ:15) + (margin-left: 15 (next card))
         
     }else {
         console.log('Esta es la última canción');
     }
-
+    
 }
 
 //Método que reproduce la canción anterior
@@ -303,10 +357,53 @@ function previousSong(){
 
     if(indexSong+1 > 1){
         const result = songsList[indexSong-1];
-        render(result, indexSong-1);
-        containerCards.scrollLeft -= 250;
+        observerFunction(indexSong, 'prev');
     }else {
         console.log('Esta es la primer canción');
+    }
+
+    prevStatus = true;
+}
+
+function observerFunction(index, direction){
+    
+    firstTime = true;
+    const obtenerDistancia = (entradas, observer) => {
+        entradas.forEach((entrada) => {
+            if(entrada.isIntersecting){
+                if(firstTime){
+                    const portionViewed = entrada.intersectionRatio*220;
+                    if(direction=='next'){
+                        const newWidth = 205-portionViewed;
+                        containerCards.scrollLeft += newWidth;
+                    }else if (direction=='prev'){
+                        const newWidth = 225-portionViewed;
+                        containerCards.scrollLeft -= newWidth;
+                    }
+                    firstTime = false;
+                }
+                
+                if(entrada.intersectionRatio == 1.00){
+                    observer.unobserve(entrada.target);
+                }
+                    
+            }
+        });
+    }
+
+    const observer = new IntersectionObserver(obtenerDistancia, {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: [0.00, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09,0.10, 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20,
+                    0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.30, 0.31, 0.32, 0.33, 0.34, 0.35, 0.36, 0.37, 0.38, 0.39, 0.40,
+                    0.41, 0.42, 0.43, 0.44, 0.45, 0.46, 0.47, 0.48, 0.49, 0.50, 0.51, 0.52, 0.53, 0.54, 0.55, 0.56, 0.57, 0.58, 0.59, 0.60,
+                    0.61, 0.62, 0.63, 0.64, 0.65, 0.66, 0.67, 0.68, 0.69, 0.70, 0.71, 0.72, 0.73, 0.74, 0.75, 0.76, 0.77, 0.78, 0.79, 0.80,
+                    0.81, 0.82, 0.83, 0.84, 0.85, 0.86, 0.87, 0.88, 0.89, 0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98,0.99, 1.00]
+    });
+    if(direction=='next'){
+        observer.observe(container.children[index+1]);
+    }else if (direction=='prev'){
+        observer.observe(container.children[index-1]);
     }
 
 }
